98f227f6cd43926acc6a9341fc45382490d1dac0
Thoma Biguères <t.bigueres@gmail.com>
2014-12-17 19:09:34 +0100

    [*] MO - ebay : fixed saveCategories too many conditions

--- ajax/saveStoreCategories.php
+++ ajax/saveStoreCategories.php
@@ -28,7 +28,7 @@ include(dirname(__FILE__).'/../../../config/config.inc.php');
 require_once(dirname(__FILE__).'/../../../init.php');
 include('../ebay.php');
 
-if (!Tools::getValue('token') || Tools::getValue('token') != Configuration::get('EBAY_SECURITY_TOKEN'))
+if (Tools::getValue('token') != Configuration::get('EBAY_SECURITY_TOKEN'))
 	die('ERROR: Invalid Token');
 
 $ebay = new eBay();
