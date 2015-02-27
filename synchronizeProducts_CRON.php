6153cdf0f7f8aaec9837ae2783214c5ef22fcd3b
De Sousa Kevin <kdesousa@202-ecommerce.com>
2015-02-27 14:28:03 +0100

    [-] MO - eBay : Cron task

--- synchronizeProducts_CRON.php
+++ synchronizeProducts_CRON.php
@@ -25,6 +25,7 @@
  */
 
 include(dirname(__FILE__) . '/../../config/config.inc.php');
+include(dirname(__FILE__) . '/../../init.php');
 include(dirname(__FILE__) . '/ebay.php');
 
 class ebaySynchronizeProductsTask extends Ebay {
