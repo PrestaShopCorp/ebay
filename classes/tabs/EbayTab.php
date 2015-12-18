2cdd01305732bfd4b1586cc46909917364fe8184
ebaudet <ebaudet@202-ecommerce.com>
2015-12-18 11:01:34 +0100

    [-] MO : norme fix

--- classes/tabs/EbayTab.php
+++ classes/tabs/EbayTab.php
@@ -18,54 +18,57 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to http://www.prestashop.com for more information.
  *
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2007-2015 PrestaShop SA
- *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2015 PrestaShop SA
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
 
 class EbayTab
 {
-	protected $ebay;
-	protected $smarty;
-	protected $ebay_profile;
-	protected $context;
-	protected $path;
-	
-	function __construct($ebay, $smarty, $context = null, $path = null)
-	{
-		$this->ebay = $ebay;
-		$this->ebay_profile = $ebay->ebay_profile;
-		$this->smarty = $smarty;
-		$this->context = $context;
-		$this->path = $path;
-	}
-	
-	protected function display($template, $template_vars)
-	{
-		$this->smarty->assign($template_vars);
-		return $this->ebay->display(dirname(__FILE__).'/../../ebay.php', '/views/templates/hook/'.$template);
-	}
-	
-	protected function _getUrl($extra_vars = array())
-	{
-		$url_vars = array(
-			'configure' => Tools::getValue('configure'),
-			'token' => Tools::getValue('token'),
-			'tab_module' => Tools::getValue('tab_module'),
-			'module_name' => Tools::getValue('module_name'),
-		);
+    /** @var Ebay $ebay */
+    protected $ebay;
+    protected $smarty;
+    /** @var EbayProfile $ebay_profile */
+    protected $ebay_profile;
+    /** @var Context $context */
+    protected $context;
+    protected $path;
 
-		return 'index.php?'.http_build_query(array_merge($url_vars, $extra_vars));
-	}
+    function __construct($ebay, $smarty, $context = null, $path = null)
+    {
+        $this->ebay = $ebay;
+        $this->ebay_profile = $ebay->ebay_profile;
+        $this->smarty = $smarty;
+        $this->context = $context;
+        $this->path = $path;
+    }
 
-	/**
-	 * Returns the module url
-	 *
-   **/
-	protected function _getModuleUrl()
-	{
-		return Tools::getShopDomain(true).__PS_BASE_URI__.'modules/ebay/';
-	}
-	
+    protected function display($template, $template_vars)
+    {
+        $this->smarty->assign($template_vars);
+
+        return $this->ebay->display(dirname(__FILE__) . '/../../ebay.php', '/views/templates/hook/' . $template);
+    }
+
+    protected function _getUrl($extra_vars = array())
+    {
+        $url_vars = array(
+            'configure'   => Tools::getValue('configure'),
+            'token'       => Tools::getValue('token'),
+            'tab_module'  => Tools::getValue('tab_module'),
+            'module_name' => Tools::getValue('module_name'),
+        );
+
+        return 'index.php?' . http_build_query(array_merge($url_vars, $extra_vars));
+    }
+
+    /**
+     * Returns the module url
+     *
+     **/
+    protected function _getModuleUrl()
+    {
+        return Tools::getShopDomain(true) . __PS_BASE_URI__ . 'modules/ebay/';
+    }
 }
