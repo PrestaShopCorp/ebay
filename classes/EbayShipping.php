c076464da74ad91f5c9ce3f8b47d2727805bcbda
ebaudet <ebaudet@202-ecommerce.com>
2015-12-21 15:55:30 +0100

    [-] MO - ebay : norme fix EbayShipping

--- classes/EbayShipping.php
+++ classes/EbayShipping.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 2007-2015 PrestaShop
  *
@@ -18,159 +19,159 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to http://www.prestashop.com for more information.
  *
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2007-2015 PrestaShop SA
- *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2015 PrestaShop SA
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
-
 class EbayShipping
 {
+    public static function getPsCarrierByEbayCarrier($id_ebay_profile, $ebay_carrier)
+    {
+        return Db::getInstance()->getValue('SELECT `ps_carrier`
+			FROM `' . _DB_PREFIX_ . 'ebay_shipping`
+			WHERE `id_ebay_profile` = ' . (int)$id_ebay_profile . '
+			AND `ebay_carrier` = \'' . pSQL($ebay_carrier) . '\'');
+    }
 
-	public static function getPsCarrierByEbayCarrier($id_ebay_profile, $ebay_carrier)
-	{
-		return Db::getInstance()->getValue('SELECT `ps_carrier`
-			FROM `'._DB_PREFIX_.'ebay_shipping`
-			WHERE `id_ebay_profile` = '.(int)$id_ebay_profile.' 
-			AND `ebay_carrier` = \''.pSQL($ebay_carrier).'\'');
-	}
-
-	public static function getNationalShippings($id_ebay_profile, $id_product = null)
-	{
-		$shippings = Db::getInstance()->ExecuteS('SELECT *
-			FROM '._DB_PREFIX_.'ebay_shipping 
-			WHERE `id_ebay_profile` = '.(int)$id_ebay_profile.' 
+    public static function getNationalShippings($id_ebay_profile, $id_product = null)
+    {
+        $shippings = Db::getInstance()->ExecuteS('SELECT *
+			FROM ' . _DB_PREFIX_ . 'ebay_shipping
+			WHERE `id_ebay_profile` = ' . (int)$id_ebay_profile . '
 			AND international = 0');
 
-		//Check if product can be shipped because of weight or dimension
-		if($id_product)
-		{
-			$exclude = array();
+        //Check if product can be shipped because of weight or dimension
+        if ($id_product) {
+            $exclude = array();
 
-			foreach ($shippings as $key => $value) 
-			{
-				$carrier = new Carrier($value['ps_carrier']);
-				$product = new Product($id_product);
-				if($carrier->range_behavior)
-				{
-					if (($carrier->getShippingMethod() == Carrier::SHIPPING_METHOD_WEIGHT
-						&& (!Carrier::checkDeliveryPriceByWeight($carrier->id, $product->weight, $value['id_zone'])))
-						|| ($carrier->getShippingMethod() == Carrier::SHIPPING_METHOD_PRICE
-						&& (!Carrier::checkDeliveryPriceByPrice($carrier->id, $product->weight, $value['id_zone'], Configuration::get('PS_CURRENCY_DEFAULT')))))
-					{
-						$exclude[] = $key;
-					}
-				}
+            foreach ($shippings as $key => $value) {
+                $carrier = new Carrier($value['ps_carrier']);
+                $product = new Product($id_product);
+                if ($carrier->range_behavior) {
+                    if (($carrier->getShippingMethod() == Carrier::SHIPPING_METHOD_WEIGHT
+                            && (!Carrier::checkDeliveryPriceByWeight($carrier->id, $product->weight,
+                                $value['id_zone'])))
+                        || ($carrier->getShippingMethod() == Carrier::SHIPPING_METHOD_PRICE
+                            && (!Carrier::checkDeliveryPriceByPrice($carrier->id, $product->weight, $value['id_zone'],
+                                Configuration::get('PS_CURRENCY_DEFAULT'))))
+                    ) {
+                        $exclude[] = $key;
+                    }
+                }
 
-				if(($carrier->max_width < $product->width && $carrier->max_width != 0) || ($carrier->max_height < $product->height && $carrier->max_height != 0) || ($carrier->max_depth < $product->depth && $carrier->max_depth != 0))
-				{
-						$exclude[] = $key;
-						continue;
-				}
-			}
+                if (($carrier->max_width < $product->width && $carrier->max_width != 0) || ($carrier->max_height < $product->height && $carrier->max_height != 0) || ($carrier->max_depth < $product->depth && $carrier->max_depth != 0)) {
+                    $exclude[] = $key;
+                    continue;
+                }
+            }
 
-			$exclude = array_unique($exclude);
-			foreach ($exclude as $key_to_exclude) {
-				unset($shippings[$key_to_exclude]);
-			}
-		}
+            $exclude = array_unique($exclude);
+            foreach ($exclude as $key_to_exclude) {
+                unset($shippings[$key_to_exclude]);
+            }
+        }
 
-		if ($id_product && version_compare(_PS_VERSION_, '1.5', '>'))
-		{
-			$shippings_product = Db::getInstance()->ExecuteS('SELECT id_carrier_reference as ps_carrier
-			FROM '._DB_PREFIX_.'product_carrier WHERE id_product = '.(int)$id_product);
-			if(count($shippings_product) > 0)
-			{
-				if(array_intersect_assoc($shippings, $shippings_product))
-					$shippings = array_intersect_assoc($shippings, $shippings_product);
-			}
-		}
+        if ($id_product && version_compare(_PS_VERSION_, '1.5', '>')) {
+            $shippings_product = Db::getInstance()->ExecuteS('SELECT id_carrier_reference as ps_carrier
+			FROM ' . _DB_PREFIX_ . 'product_carrier WHERE id_product = ' . (int)$id_product);
+            if (count($shippings_product) > 0) {
+                if (array_intersect_assoc($shippings, $shippings_product)) {
+                    $shippings = array_intersect_assoc($shippings, $shippings_product);
+                }
+            }
+        }
 
-		return $shippings;
-	}
+        return $shippings;
+    }
 
-	public static function internationalShippingsHaveZone($shippings)
-	{
-		foreach ($shippings as $shipping) {
-			if(!Db::getInstance()->getValue('SELECT * FROM '._DB_PREFIX_.'ebay_shipping_international_zone WHERE id_ebay_shipping = '.(int)$shipping['id_ebay_shipping']))
-				return false;
-		}
-		return true;
-	}
+    public static function internationalShippingsHaveZone($shippings)
+    {
+        foreach ($shippings as $shipping) {
+            if (!Db::getInstance()->getValue('SELECT * FROM ' . _DB_PREFIX_ . 'ebay_shipping_international_zone WHERE id_ebay_shipping = ' . (int)$shipping['id_ebay_shipping'])) {
+                return false;
+            }
+        }
 
-	public static function getInternationalShippings($id_ebay_profile, $id_product = null)
-	{
-		$shippings = Db::getInstance()->ExecuteS('SELECT *
-			FROM '._DB_PREFIX_.'ebay_shipping 
-			WHERE `id_ebay_profile` = '.(int)$id_ebay_profile.' 
+        return true;
+    }
+
+    public static function getInternationalShippings($id_ebay_profile, $id_product = null)
+    {
+        $shippings = Db::getInstance()->ExecuteS('SELECT *
+			FROM ' . _DB_PREFIX_ . 'ebay_shipping
+			WHERE `id_ebay_profile` = ' . (int)$id_ebay_profile . '
 			AND international = 1');
-		
-		//Check if product can be shipped because of weight or dimension
-		if($id_product)
-		{
-			$exclude = array();
 
-			foreach ($shippings as $key => $value) 
-			{
-				$carrier = new Carrier($value['ps_carrier']);
-				$product = new Product($id_product);
-				if($carrier->range_behavior)
-				{
-					if (($carrier->getShippingMethod() == Carrier::SHIPPING_METHOD_WEIGHT
-						&& (!Carrier::checkDeliveryPriceByWeight($carrier->id, $product->weight, $value['id_zone'])))
-						|| ($carrier->getShippingMethod() == Carrier::SHIPPING_METHOD_PRICE
-						&& (!Carrier::checkDeliveryPriceByPrice($carrier->id, $product->weight, $value['id_zone'], Configuration::get('PS_CURRENCY_DEFAULT')))))
-					{
-						$exclude[] = $key;
-					}
-				}
+        //Check if product can be shipped because of weight or dimension
+        if ($id_product) {
+            $exclude = array();
 
-				if(($carrier->max_width < $product->width && $carrier->max_width != 0) || ($carrier->max_height < $product->height && $carrier->max_height != 0) || ($carrier->max_depth < $product->depth && $carrier->max_depth != 0))
-				{
-						$exclude[] = $key;
-						continue;
-				}
-			}
+            foreach ($shippings as $key => $value) {
+                $carrier = new Carrier($value['ps_carrier']);
+                $product = new Product($id_product);
+                if ($carrier->range_behavior) {
+                    if (($carrier->getShippingMethod() == Carrier::SHIPPING_METHOD_WEIGHT
+                            && (!Carrier::checkDeliveryPriceByWeight($carrier->id, $product->weight,
+                                $value['id_zone'])))
+                        || ($carrier->getShippingMethod() == Carrier::SHIPPING_METHOD_PRICE
+                            && (!Carrier::checkDeliveryPriceByPrice($carrier->id, $product->weight, $value['id_zone'],
+                                Configuration::get('PS_CURRENCY_DEFAULT'))))
+                    ) {
+                        $exclude[] = $key;
+                    }
+                }
 
-			$exclude = array_unique($exclude);
-			foreach ($exclude as $key_to_exclude) {
-				unset($shippings[$key_to_exclude]);
-			}
-		}
-		
-		if ($id_product && version_compare(_PS_VERSION_, '1.5', '>'))
-		{
-			$shippings_product = Db::getInstance()->ExecuteS('SELECT id_carrier_reference as ps_carrier
-			FROM '._DB_PREFIX_.'product_carrier WHERE id_product = '.(int)$id_product);
-			if(count($shippings_product) > 0)
-			{
-				if(array_intersect_assoc($shippings, $shippings_product))
-					$shippings = array_intersect_assoc($shippings, $shippings_product);
-			}
-		}
+                if (($carrier->max_width < $product->width && $carrier->max_width != 0) || ($carrier->max_height < $product->height && $carrier->max_height != 0) || ($carrier->max_depth < $product->depth && $carrier->max_depth != 0)) {
+                    $exclude[] = $key;
+                    continue;
+                }
+            }
 
-		return $shippings;
-	}
+            $exclude = array_unique($exclude);
+            foreach ($exclude as $key_to_exclude) {
+                unset($shippings[$key_to_exclude]);
+            }
+        }
 
-	public static function getNbNationalShippings($id_ebay_profile)
-	{
-		return Db::getInstance()->getValue('SELECT count(*)
-			FROM '._DB_PREFIX_.'ebay_shipping 
+        if ($id_product && version_compare(_PS_VERSION_, '1.5', '>')) {
+            $shippings_product = Db::getInstance()->ExecuteS('SELECT id_carrier_reference as ps_carrier
+			FROM ' . _DB_PREFIX_ . 'product_carrier WHERE id_product = ' . (int)$id_product);
+            if (count($shippings_product) > 0) {
+                if (array_intersect_assoc($shippings, $shippings_product)) {
+                    $shippings = array_intersect_assoc($shippings, $shippings_product);
+                }
+            }
+        }
+
+        return $shippings;
+    }
+
+    public static function getNbNationalShippings($id_ebay_profile)
+    {
+        return Db::getInstance()->getValue('SELECT count(*)
+			FROM ' . _DB_PREFIX_ . 'ebay_shipping
 			WHERE `international` = 0
-			AND `id_ebay_profile` = '.(int)$id_ebay_profile);
-	}
+			AND `id_ebay_profile` = ' . (int)$id_ebay_profile);
+    }
 
-	public static function getNbInternationalShippings($id_ebay_profile)
-	{
-		return Db::getInstance()->getValue('SELECT count(*)
-			FROM '._DB_PREFIX_.'ebay_shipping 
+    public static function getNbInternationalShippings($id_ebay_profile)
+    {
+        return Db::getInstance()->getValue('SELECT count(*)
+			FROM ' . _DB_PREFIX_ . 'ebay_shipping
 			WHERE international = 1
-			AND `id_ebay_profile` = '.(int)$id_ebay_profile);
-	}
+			AND `id_ebay_profile` = ' . (int)$id_ebay_profile);
+    }
 
-	public static function insert($id_ebay_profile, $ebay_carrier, $ps_carrier, $extra_fee, $id_zone, $international = false)
-	{
-		$sql = 'INSERT INTO `'._DB_PREFIX_.'ebay_shipping` (
+    public static function insert(
+        $id_ebay_profile,
+        $ebay_carrier,
+        $ps_carrier,
+        $extra_fee,
+        $id_zone,
+        $international = false
+    ) {
+        $sql = 'INSERT INTO `' . _DB_PREFIX_ . 'ebay_shipping` (
 			`id_ebay_profile`, 
 			`ebay_carrier`, 
 			`ps_carrier`, 
@@ -179,33 +180,34 @@ class EbayShipping
 			`id_zone`
 			)
 			VALUES(
-			\''.(int)$id_ebay_profile.'\',
-			\''.pSQL($ebay_carrier).'\',
-			\''.(int)$ps_carrier.'\',
-			\''.(float)$extra_fee.'\',
-			\''.(int)$international.'\', 
-			\''.(int)$id_zone.'\')';
+			\'' . (int)$id_ebay_profile . '\',
+			\'' . pSQL($ebay_carrier) . '\',
+			\'' . (int)$ps_carrier . '\',
+			\'' . (float)$extra_fee . '\',
+			\'' . (int)$international . '\',
+			\'' . (int)$id_zone . '\')';
 
-		DB::getInstance()->Execute($sql);
-	}
+        DB::getInstance()->Execute($sql);
+    }
 
-	public static function truncate($id_ebay_profile)
-	{
-		return Db::getInstance()->Execute('DELETE FROM '._DB_PREFIX_.'ebay_shipping
-			WHERE `id_ebay_profile` = '.(int)$id_ebay_profile);
-	}
+    public static function truncate($id_ebay_profile)
+    {
+        return Db::getInstance()->Execute('DELETE FROM ' . _DB_PREFIX_ . 'ebay_shipping
+			WHERE `id_ebay_profile` = ' . (int)$id_ebay_profile);
+    }
 
-	public static function getLastShippingId($id_ebay_profile)
-	{
-		return Db::getInstance()->getValue('SELECT id_ebay_shipping
-			FROM '._DB_PREFIX_.'ebay_shipping
-			WHERE `id_ebay_profile` = '.(int)$id_ebay_profile.'
+    public static function getLastShippingId($id_ebay_profile)
+    {
+        return Db::getInstance()->getValue('SELECT id_ebay_shipping
+			FROM ' . _DB_PREFIX_ . 'ebay_shipping
+			WHERE `id_ebay_profile` = ' . (int)$id_ebay_profile . '
 			ORDER BY id_ebay_shipping DESC');
-	}
+    }
 
-	public static function updatePsCarrier($old_ps_carrier, $new_ps_carrier){
-		return Db::getInstance()->Execute('UPDATE `'._DB_PREFIX_.'ebay_shipping` 
-				SET `ps_carrier` = '.(int)$new_ps_carrier.'
-				WHERE `ps_carrier` = '.(int)$old_ps_carrier);
-	}
+    public static function updatePsCarrier($old_ps_carrier, $new_ps_carrier)
+    {
+        return Db::getInstance()->Execute('UPDATE `' . _DB_PREFIX_ . 'ebay_shipping`
+				SET `ps_carrier` = ' . (int)$new_ps_carrier . '
+				WHERE `ps_carrier` = ' . (int)$old_ps_carrier);
+    }
 }
