d4d16451df7c37bc687d1f5f0f8bc8d7932855ca
ebaudet <ebaudet@202-ecommerce.com>
2016-02-08 11:20:25 +0100

    [*] MO : Ebay - reload list specifics and conditions after upgrade 1.12.2

--- upgrade/Upgrade-1.12.2.php
+++ upgrade/Upgrade-1.12.2.php
@@ -34,5 +34,7 @@ function upgrade_module_1_12_2($module)
         $module->setConfiguration('EBAY_SYNCHRONIZE_EAN', 'EAN');
     }
 
+    $module->ebay_profile->deleteConfigurationByName('EBAY_SPECIFICS_LAST_UPDATE');
+
     return true;
 }
