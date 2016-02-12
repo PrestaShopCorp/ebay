906a19a884b89e22536c37a569719d5904a56548
Thoma Biguères <tbigueres@202-ecommerce.com>
2016-02-12 10:31:42 +0100

    [*] MO - ebay : remove unecessary function

--- ebay.php
+++ ebay.php
@@ -2108,31 +2108,6 @@ class Ebay extends Module
 
     }
 
-    private function __postProcessDownloadLog()
-    {
-        $full_path = _PS_MODULE_DIR_.'ebay/log/request.txt';
-        if (file_exists($full_path)) {
-            $file_name = basename($full_path);
-
-            $date = gmdate(DATE_RFC1123);
-
-            header('Pragma: public');
-            header('Cache-Control: must-revalidate, pre-check=0, post-check=0, max-age=0');
-
-            header('Content-Tranfer-Encoding: none');
-            header('Content-Length: '.filesize($full_path));
-            header('Content-MD5: '.base64_encode(md5_file($full_path)));
-            header('Content-Type: application/octetstream; name="'.$file_name.'"');
-            header('Content-Disposition: attachment; filename="'.$file_name.'"');
-
-            header('Date: '.$date);
-            header('Expires: '.gmdate(DATE_RFC1123, time() + 1));
-            header('Last-Modified: '.gmdate(DATE_RFC1123, filemtime($full_path)));
-
-            readfile($full_path);
-            exit;
-        }
-    }
 
     ############################################################################################################
     # Logger // Debug
